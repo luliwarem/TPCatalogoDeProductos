af5fbd88a358578760e0c86a890774850ec81bb9
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-02 15:15:36 -0300

    hola jesi

--- src/Home.js
+++ src/Home.js
@@ -3,30 +3,17 @@ import React, { Component, useContext, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import { MyContext } from "./MyContext";
-import axios from "axios"
-
-
+import axios from "axios";
 
 export default function Home() {
-
-  const {productos, setProductos} = useContext(MyContext)
-
-  useEffect(() => {
-    async function getProductos(){
-      const response = await axios.get(
-        `https://dummyjson.com/products`
-      );
-      setProductos(response.data);
-    }
-    getProductos();
-  }, []);
-
+  const { productos, setProductos } = useContext(MyContext);
 
   return (
     <div class="container">
       <div className="center-div">
+        {console.log(productos)}
         <Carousel
           class="carousel"
           showStatus={false}
@@ -40,64 +27,63 @@ export default function Home() {
           <div>
             <img src="https://upload.wikimedia.org/wikipedia/commons/1/18/Dog_Breeds.jpg" />
           </div>
-          
         </Carousel>
       </div>
       <div className="cards">
         <div class="card">
           <div className="card-image">
-            <img
+            {/*<img
               width="300px"
               height="auto"
               src={productos[1].images[1]}
-            ></img>
+  ></img>*/}
           </div>
           <div class="card__content">
             <p class="card__title">{productos[1].title}</p>
-            <p class="card__description">
-             {productos[1].description}
-            </p>
-            
+            <p class="card__description">{productos[1].description}</p>
+
             <button>
-              <span><Link to="/detalle/1"> Detalle</Link></span>
+              <span>
+                <Link to="/detalle/1"> Detalle</Link>
+              </span>
             </button>
           </div>
         </div>
         <div class="card">
           <div className="card-image">
-            <img
+            {/*<img
               width="300px"
               height="auto"
-              src={productos[2].images[1]}
-            ></img>
+              src={productos[1].images[1]}
+  ></img>*/}
           </div>
           <div class="card__content">
             <p class="card__title">{productos[2].title}</p>
-            <p class="card__description">
-              {productos[2].description}
-            </p>
-            
+            <p class="card__description">{productos[2].description}</p>
+
             <button>
-              <span><Link to="/detalle/2"> Detalle</Link></span>
+              <span>
+                <Link to="/detalle/2"> Detalle</Link>
+              </span>
             </button>
           </div>
         </div>
         <div class="card">
           <div className="card-image">
-            <img
+            {/*<img
               width="300px"
               height="auto"
-              src={productos[3].images[1]}
-            ></img>
+              src={productos[1].images[1]}
+  ></img>*/}
           </div>
           <div class="card__content">
             <p class="card__title">{productos[3].title}</p>
-            <p class="card__description">
-              {productos[3].description}
-            </p>
-            
+            <p class="card__description">{productos[3].description}</p>
+
             <button>
-              <span><Link to="/detalle/3"> Detalle</Link></span>
+              <span>
+                <Link to="/detalle/3"> Detalle</Link>
+              </span>
             </button>
           </div>
         </div>
@@ -105,54 +91,57 @@ export default function Home() {
       <div className="cards">
         <div class="card">
           <div className="card-image">
-            <img
+            {/*<img
               width="300px"
               height="auto"
-              src={productos[4].images[1]}
-            ></img>
+              src={productos[1].images[1]}
+  ></img>*/}
           </div>
           <div class="card__content">
             <p class="card__title">{productos[4].title}</p>
-            <p class="card__description">{productos[4].description}
-            </p>
-            
+            <p class="card__description">{productos[4].description}</p>
+
             <button>
-              <span><Link to="/detalle/1"> Detalle</Link></span>
+              <span>
+                <Link to="/detalle/4"> Detalle</Link>
+              </span>
             </button>
           </div>
         </div>
         <div class="card">
           <div className="card-image">
-            <img
+            {/*<img
               width="300px"
               height="auto"
-              src={productos[5].images[1]}
-              ></img>
-            </div>
-            <div class="card__content">
-              <p class="card__title">{productos[5].title}</p>
-              <p class="card__description">{productos[5].description}
-              </p>
-            
+              src={productos[1].images[1]}
+  ></img>*/}
+          </div>
+          <div class="card__content">
+            <p class="card__title">{productos[5].title}</p>
+            <p class="card__description">{productos[5].description}</p>
+
             <button>
-              <span><Link to="/detalle/1"> Detalle</Link></span>
+              <span>
+                <Link to="/detalle/5"> Detalle</Link>
+              </span>
             </button>
           </div>
         </div>
         <div class="card">
           <div className="card-image">
-            <img
+            {/*<img
               width="300px"
               height="auto"
-              src={productos[6].images[1]}
-            ></img>
+              src={productos[1].images[1]}
+  ></img>*/}
           </div>
           <div class="card__content">
             <p class="card__title">{productos[6].title}</p>
-            <p class="card__description">{productos[6].description}
-            </p>
+            <p class="card__description">{productos[6].description}</p>
             <button>
-              <span><Link to="/detalle/1"> Detalle</Link></span>
+              <span>
+                <Link to="/detalle/6"> Detalle</Link>
+              </span>
             </button>
           </div>
         </div>
--- src/MyContext.js
+++ src/MyContext.js
@@ -1,36 +1,36 @@
-import React, {createContext, useState, useEffect, useContext} from "react";
-import axios from 'axios';
+import React, { createContext, useState, useEffect, useContext } from "react";
+import axios from "axios";
 
 export const MyContext = createContext("");
 
-const ObtenerProductos =({children})=>{
-    const [productos, setProductos] = useState([])
+const ObtenerProductos = ({ children }) => {
+  const [productos, setProductos] = useState([]);
 
-    useEffect(() => {
-        async function getProductos(){
-        const {response} = await axios.get(
-            `https://dummyjson.com/products`
-        );
-        setProductos(response);
-        }
-        getProductos();
-    }, []);
-    
-    return(
-        <MyContext.Provider value={{
-            productos
-          }}>
-            {children}
-        </MyContext.Provider>
-    )
-}
+  useEffect(() => {
+    async function getProductos() {
+      const response = await axios
+        .get(`https://dummyjson.com/products`)
+        .then((response) => response.json())
+        .then((response) => setProductos(response));
+    }
+    getProductos();
+  }, []);
+
+  return (
+    <MyContext.Provider
+      value={{
+        productos,
+      }}
+    >
+      {children}
+    </MyContext.Provider>
+  );
+};
 
 export function Contexto() {
-    const context = useContext(ObtenerProductos);
-    if (context === undefined) {
-      throw new Error("Context must be used within a Provider");
-    }
-    return context;
+  const context = useContext(ObtenerProductos);
+  if (context === undefined) {
+    throw new Error("Context must be used within a Provider");
   }
-  
-
+  return context;
+}
